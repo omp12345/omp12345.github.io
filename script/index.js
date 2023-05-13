f6f95161d2cf506b78785f9a289e8fed1d784bb9
Om Prakash Mishra <opsgp1999@gmail.com>
2023-05-13 00:27:07 -0700

    ok

---
+++ script/index.js
@@ -0,0 +1,39 @@
+
+
+
+// console.log("hello")
+
+document.querySelector(".cross").style.display="none"
+document.querySelector(".hemburger").addEventListener("click",()=>{
+    document.querySelector(".sidebar").classList.toggle("sidebargo")
+    if(document.querySelector(".sidebar").classList.contains("sidebargo")){
+        document.querySelector(".hem").style.display='inline'
+        document.querySelector(".cross").style.display="none"
+    }
+    else{
+        document.querySelector(".hem").style.display="none"
+        setTimeout(()=>{
+            document.querySelector(".cross").style.display="inline"
+        },400)
+       
+    }
+})
+
+let ResumeBtn=document.getElementById("homeBtndiv")
+ResumeBtn.onclick=()=>{
+  window.open("./Om-Prakash-Mishra-Resume.pdf", "_blank")
+}
+
+
+let navResume=document.getElementById("navResume")
+navResume.onclick=()=>{
+  window.open("./Om-Prakash-Mishra-Resume.pdf", "_blank")
+}
+let sideR=document.getElementById("sideResume")
+sideR.onclick=()=>{
+  window.open("./Om-Prakash-Mishra-Resume.pdf", "_blank")
+}
+let aboutR=document.getElementById("aboutResumepdf")
+aboutR.onclick=()=>{
+  window.open("./Om-Prakash-Mishra-Resume.pdf", "_blank")
+}
