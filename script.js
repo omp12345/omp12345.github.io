532bbad96ab3c7daff0dd775d7405cac6751f2a8
omp12345 <112754710+omp12345@users.noreply.github.com>
2023-10-11 13:08:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -158,7 +158,7 @@ const elements = document.getElementsByClassName("home-content");
 for (let i = 0; i < elements.length; i++) {
   elements[i].addEventListener("mouseenter", () => {
     const text =
-      "Hii Recruiter  I am Om Prakash Mishra I am web developer this is my portpholio You can Check it and contact me to join our Team My contact number is 7275162886";
+      "Hii Recruiter  I am Om Prakash Mishra I am web developer this is my portpholio You can Check it and contact me to join our Team My email is opsgp1999@gmail.com";
     speakTextWithIndianAccent(text, indianAccentVoiceName);
   });
 
@@ -166,3 +166,70 @@ for (let i = 0; i < elements.length; i++) {
     window.speechSynthesis.cancel();
   });
 }
+// Check if the SpeechSynthesis API is supported by the browser
+if ('speechSynthesis' in window) {
+  // Get the resume button element
+  const resumeButton = document.getElementById('resume-button-1');
+  const resumeButton1 = document.getElementById('resume-button-2');
+
+  // Create a function to speak the text when the button is hovered
+  function speakText(text) {
+   
+   
+    const speech = new SpeechSynthesisUtterance(text);
+    speechSynthesis.speak(speech);
+  }
+
+  // Add a mouseover event listener to the button
+  resumeButton.addEventListener('mouseover', ()=>{
+    speakText("Hey recruter plese download resume")
+  });
+  resumeButton.addEventListener("mouseleave", () => {
+    window.speechSynthesis.cancel();
+  });
+  resumeButton1.addEventListener('mouseover', ()=>{
+    speakText("click to download resume")
+  });
+}
+// Function to speak a message
+
+// Function to speak the name of a skill
+function speakSkillName(skillName,x) {
+ let message=undefined
+  if(x){
+     message = ` i rated myself in ${skillName} is ${x}`;
+  }else if(skillName=="Thunder Client"){
+    message=` i rated myself in ${skillName} is ${8}`
+  }
+  else if(skillName=="VSCode"){
+    message=` i rated myself in ${skillName} is ${10}`
+  }
+  else if(skillName=="Bootstrap"){
+    message=` I am learning ${skillName}`
+  }else{
+    message=` thank You}`
+  }
+  const speech = new SpeechSynthesisUtterance(message);
+  speechSynthesis.speak(speech);
+}
+
+// Get all the skills-card elements
+const skillButtons = document.querySelectorAll('.skills-card.tag-btn');
+
+// Add a mouseover event listener to each skills-card button
+skillButtons.forEach((button,index) => {
+  const arr=[7,6.5,8,7,7.5,7,6.5,6,7]
+  button.addEventListener('mouseover', () => {
+   
+    const skillName = button.querySelector('.skills-card-name').textContent;
+    speakSkillName(skillName,arr[index])
+
+    // Add a mouseout event listener to remove the voice when hovering stops
+    button.addEventListener('mouseout', () => {
+      speechSynthesis.cancel(); // Stop the speech when mouseout event is triggered
+    });
+  });
+});
+
+// Function to check if the user has reached the end of the page
+
