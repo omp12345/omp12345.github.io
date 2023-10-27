37e57028922e18f7597e2b453f0968f1add61ffe
omp12345 <opsgp1999@gmail.com>
2023-10-27 06:00:02 -0700

    ok

--- script.js
+++ script.js
@@ -8,8 +8,11 @@ menuIcon.onclick = () => {
   navbar.classList.toggle("active");
 };
 
-// *=============================== github calendar ==================================
+// *=============================== github calender ==================================
 // GitHubCalendar(".calendar", "omp12345");
+
+// // or enable responsive functionality:
+// GitHubCalendar(".calendar", "omp12345", { responsive: true });
 
 // *=============================== resume opening in a new tab ==================================
 
@@ -46,7 +49,6 @@ window.onscroll = () => {
       });
     }
   });
-
   // *=============================== sticky navbar ==================================
 
   let header = document.querySelector("header");
@@ -76,7 +78,8 @@ ScrollReveal().reveal(
 ScrollReveal().reveal(".home-content h1", { origin: "left" });
 ScrollReveal().reveal(".home-content p, .about-content", { origin: "left" });
 
-// *=============================== typed js ==================================
+// *===============================  typed js==================================
+
 const typed = new Typed(".multiple-text", {
   strings: [
     "Full Stack Web Developer",
@@ -109,11 +112,119 @@ GitHubCalendar(".calendar", "omp12345", {
   },
 }).then((r) => r.text());
 
-// *=============================== End of the page marker ==================================
+function speakText(text) {
+  const synth = window.speechSynthesis;
+  const utterance = new SpeechSynthesisUtterance(text);
+  synth.speak(utterance);
+}
+
+// const projectCards = document.getElementsByClassName("project-card");
+
+// for (let i = 0; i < projectCards.length; i++) {
+//   const projectCard = projectCards[i];
+//   const p1data = projectCard.querySelector(".p1").innerText;
+
+//   projectCard.addEventListener("mouseenter", () => {
+//     speakText(p1data);
+//   });
+
+//   projectCard.addEventListener("mouseleave", () => {
+//     window.speechSynthesis.cancel();
+//   });
+// }
+
+function speakTextWithIndianAccent(text, voiceName) {
+  const synth = window.speechSynthesis;
+  const utterance = new SpeechSynthesisUtterance(text);
+
+  const voices = synth.getVoices();
+  const indianVoice = voices.find((voice) => voice.lang === voiceName);
+
+  if (indianVoice) {
+    utterance.voice = indianVoice;
+  }
+
+  synth.speak(utterance);
+}
+
+const indianAccentVoiceName = "hindi";
+
+const elements = document.getElementsByClassName("home-content");
+
+for (let i = 0; i < elements.length; i++) {
+  elements[i].addEventListener("mouseenter", () => {
+    const text =
+      "Hii Recruiter  I am Om Prakash Mishra I am web developer welcome to my portpholio website";
+    speakTextWithIndianAccent(text, indianAccentVoiceName);
+  });
+
+  elements[i].addEventListener("mouseleave", () => {
+    window.speechSynthesis.cancel();
+  });
+}
+
+// if ("speechSynthesis" in window) {
+  const resumeButton = document.getElementById("resume-button-1");
+  const resumeButton1 = document.getElementById("resume-button-2");
+
+//   function speakText(text) {
+//     const speech = new SpeechSynthesisUtterance(text);
+//     speechSynthesis.speak(speech);
+//   }
+
+//   // resumeButton.addEventListener("mouseover", () => {
+//   //   speakText("click to download resume");
+//   // });
+//   resumeButton.addEventListener("mouseleave", () => {
+//     window.speechSynthesis.cancel();
+//   });
+//   // resumeButton1.addEventListener("mouseover", () => {
+//   //   speakText("click to download resume");
+//   // });
+// }
+
+function speakSkillName(skillName, x) {
+  let message = undefined;
+  if (x) {
+    message = ` i rated myself in ${skillName} is ${x}`;
+  } else if (skillName == "Thunder Client") {
+    message = ` i rated myself in ${skillName} is ${8}`;
+  } else if (skillName == "VSCode") {
+    message = ` i rated myself in ${skillName} is ${10}`;
+  } else if (skillName == "Bootstrap") {
+    message = ` I am learning ${skillName}`;
+  } else {
+    message = ` thank You}`;
+  }
+  const speech = new SpeechSynthesisUtterance(message);
+  speechSynthesis.speak(speech);
+}
+
+const skillButtons = document.querySelectorAll(".skills-card.tag-btn");
+
+skillButtons.forEach((button, index) => {
+  const arr = [7, 6.5, 8, 7, 7.5, 7, 6.5, 6, 7];
+  button.addEventListener("mouseover", () => {
+    const skillName = button.querySelector(".skills-card-name").textContent;
+    speakSkillName(skillName, arr[index]);
+
+    button.addEventListener("mouseout", () => {
+      speechSynthesis.cancel();
+    });
+  });
+});
+
+// Function to speak a message
+function speakMessage(message) {
+  const speech = new SpeechSynthesisUtterance(message);
+  speechSynthesis.speak(speech);
+}
+
 
 const targetElement = document.createElement('div');
 targetElement.id = 'end-of-page-marker';
 document.body.appendChild(targetElement);
+
 
 const options = {
   root: null,
@@ -121,10 +232,11 @@ const options = {
   threshold: 0.99, 
 };
 
+
 const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      // speakMessage("Thank You for visting, have a great time");
+      speakMessage("Thank You for visting, have a great time");
     }
   });
 }, options);
