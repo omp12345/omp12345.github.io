376dfa75f7ed54bbb12bd60e57a5e59891e7f3fa
omp12345 <opsgp1999@gmail.com>
2023-10-28 01:59:46 -0700

    ok

--- script.js
+++ script.js
@@ -93,11 +93,11 @@ function statsfun(){
   );
 }
 
-// GitHubCalendar(".calendar", "omp12345", {
-//   responsive: true,
-//   global_stats: false,
-//   tooltips: true,
-// });
+GitHubCalendar(".react-activity-calendar", "omp12345", {
+  responsive: true,
+  global_stats: false,
+  tooltips: true,
+});
 
 
 
