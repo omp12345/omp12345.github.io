c236a87010233688a67ef06d4c13f637fa065a72
omp12345 <opsgp1999@gmail.com>
2023-10-28 21:09:34 -0700

    ok

--- script.js
+++ script.js
@@ -1,68 +1,58 @@
-// *=============================== toggle icon navbar ==================================
 
 
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('#nav-menu')
+let main = document.querySelector('#menu-icon');
+let Navbar = document.querySelector('#nav-menu')
 
 menuIcon.onclick = () => {
-  menuIcon.classList.toggle('bx-x');
-  navbar.classList.toggle('active')
+  main.classList.toggle('bx-x');
+  Navbar.classList.toggle('active')
 };
 
-// *=============================== github calender ==================================
-    GitHubCalendar(".calendar", "opm12345");
-
-    // or enable responsive functionality:
-    GitHubCalendar(".calendar", "opm12345", { responsive: true });
-
-// *=============================== resume opening in a new tab ==================================
 
 
-    let resume1 = document.getElementById("resume-button-1").addEventListener("click", NewTab)
-    let resume2 = document.getElementById("resume-button-2").addEventListener("click", NewTab)
+    let resume1 = document.getElementById("resume-button-1").addEventListener("click", callnewtab)
+    let resume2 = document.getElementById("resume-button-2").addEventListener("click", callnewtab)
 
-    function NewTab() {
+    function callnewtab() {
       window.open(
         "https://drive.google.com/file/d/1MGp-0Dvv9NddxDRSWh3uLxgUT3T_-Rff/view?usp=sharing"
       );
     }
 
-// *=============================== scroll sections active link ==================================
-let sections = document.querySelectorAll('section');
-let navlinks = document.querySelectorAll('header nav a')
+let Sections = document.querySelectorAll('section');
+let Navlink = document.querySelectorAll('header nav a')
 
 window.onscroll = () => {
-  sections.forEach(sec => {
+  Sections.forEach(sec => {
     let top = window.scrollY;
     let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
     let id = sec.getAttribute('id');
 
     if(top >= offset && top < offset + height) {
-        navlinks.forEach(links => {
+        Navlink.forEach(links => {
           links.classList.remove('active');
           document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
         });
     };
 
   });
-  // *=============================== sticky navbar ==================================
+  
    
   let header = document.querySelector('header');
 
   header.classList.toggle('sticky', window.scrollY > 100);
 
-// *=============================== remove toggle icon and navbar when click navbar link(scroll) ==================================
 
 menuIcon.classList.remove('bx-x');
-navbar.classList.remove('active')
+Navbar.classList.remove('active')
 
 };
 
-// *=============================== scroll reveal ==================================
+
 
 ScrollReveal({
-  // reset: true,
+ 
   distance: '80px',
   duration: 2000,
   delay: 200
@@ -73,7 +63,6 @@ ScrollReveal().reveal('.home-img, .skills-container, .project-card, .contact for
 ScrollReveal().reveal('.home-content h1', { origin:'left'});
 ScrollReveal().reveal('.home-content p, .about-content', { origin:'left'});
 
-// *===============================  typed js==================================
 
 const typed = new Typed('.multiple-text', {
   strings: ['Full Stack Web Developer', 'Backend Developer with Node.js', 'Problem Solver'],
@@ -106,17 +95,3 @@ GitHubCalendar(".react-activity-calendar", "omp12345", {
 
 
 
-// or enable responsive functionality:
-
-
-// GitHubCalendar(".calendar", "omp12345");
-
-// // or enable responsive functionality:
-// GitHubCalendar(".calendar", "omp12345", { responsive: true });
-
-// // Use a proxy
-// GitHubCalendar(".calendar", "omp12345", {
-//    proxy (username) {
-//      return fetch(`https://your-proxy.com/github?user=${username}`)
-//    }
-// }).then(r => r.text())
